c29cab6bdf58009932eb4d136c2b2d2d008cb4ac
bethropolis <66518866+bethropolis@users.noreply.github.com>
2024-06-29 21:00:43 +0300

    Update main.go to calculate and display test statistics in the README.md

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ func main() {
 
 	testResults := readTestResults()
 
+	totalTests, passedTests, failedTests := calculateTestStats(testResults)
+
 	f, err := os.Create("index.json")
 	if err != nil {
 		log.Fatal(err)
@@ -33,19 +35,24 @@ func main() {
 
 	f2, err2 := os.Create("README.md")
 	if err2 != nil {
-		log.Fatal(err)
+		log.Fatal(err2)
 	}
 	defer f2.Close()
 
-	readme := `
+	readme := fmt.Sprintf(`
 # Miru-Repo
 
 Miru extensions repository | [Miru App Download](https://github.com/miru-project/miru-app) |
 
+[![Total - %d](https://img.shields.io/static/v1?label=Total&message=%d&style=for-the-badge&labelColor=0077b6&color=0077b6)](#)
+[![passed - %d](https://img.shields.io/static/v1?label=Passed&message=%d&style=for-the-badge&labelColor=38b000&color=38b000)](#)
+[![failed - %d](https://img.shields.io/static/v1?label=Failed&message=%d&style=for-the-badge&labelColor=da1e37&color=da1e37)](#)
+
+
 ## List
 |  Name   | Package | Version | Author | Language | Type | Test | Source |
 |  ----   | ---- | --- | ---  | ---  | --- | --- | --- |
-`
+`, totalTests, totalTests, passedTests, passedTests, failedTests, failedTests)
 
 	for _, v := range extensions {
 		url := fmt.Sprintf("[Source Code](%s)", "https://github.com/miru-project/repo/blob/main/repo/"+v["url"])
@@ -80,7 +87,7 @@ func readRepoExtensions() []map[string]string {
 		lines := strings.Split(data[0], "\n")
 		extension := make(map[string]string)
 		for _, v := range lines {
-			if v[:4] == "// @" {
+			if len(v) > 4 && v[:4] == "// @" {
 				s := strings.Split(v[4:], " ")
 				extension[s[0]] = strings.Trim(s[len(s)-1], "\r")
 			}
@@ -107,14 +114,27 @@ func readTestResults() map[string]TestResult {
 	return results
 }
 
+func calculateTestStats(testResults map[string]TestResult) (total, passed, failed int) {
+	for _, result := range testResults {
+		total++
+		if result.Status == "pass" {
+			passed++
+		} else if result.Status == "fail" {
+			failed++
+		}
+	}
+	return total, passed, failed
+}
+
 func checkTestStatus(packageName string, testResults map[string]TestResult) string {
 	result, exists := testResults[packageName]
 	if !exists {
-		return "--" 
+		return "--"
 	}
+	
 	if result.Status == "pass" {
 		return "✅"
-	}else if result.Status == "fail" {
+	} else if result.Status == "fail" {
 		return "❌"
 	}
 
